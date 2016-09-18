7284d9cba7cf12af221c9614bb4fe2f044d98dc4
j8761336 <105021026@live.asia.edu.tw>
2016-09-19 01:10:40 +0800

    Add files via upload

---
+++ dan06.java
@@ -0,0 +1,14 @@
+package dandan;
+import java.util.Scanner;
+public class dan06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn =new Scanner(System.in);
+		System.out.print("Plz enter 2 numbers!!");
+		int a =scn.nextInt();
+		int b =scn.nextInt();
+		System.out.print("PLUS=" + (a+b));
+	}
+
+}
